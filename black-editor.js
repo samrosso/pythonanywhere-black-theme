4400211bcba8b73edc1e6604abdca576477a468e
TribeLoop <99650975+TribeLoop@users.noreply.github.com>
2022-08-29 20:56:31 +0200

    Update black-editor.js

--- black-editor.js
+++ black-editor.js
@@ -1,8 +1,8 @@
 // Change ace theme
 Anywhere.Editor.editor.setTheme('ace/theme/monokai');
 
-// Change font size
-Anywhere.Editor.editor.setFontSize(20);
+// Change font size, optional
+// Anywhere.Editor.editor.setFontSize(20);
 
-// Only if the console is hidden
-//$('#id_editor').css('height', '100%');
+// CSS fix, only if the console is hidden
+// $('#id_editor').css('height', '100%');
